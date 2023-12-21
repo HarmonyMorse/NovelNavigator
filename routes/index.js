158eadfdf640c2cb4c5b0217d57ac410596b8c50
HarmonyMorse <harmonythedev@gmail.com>
2023-12-21 10:33:18 -0800

    Add routes for authentication.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,43 @@
 var express = require('express');
 var router = express.Router();
+const passport = require('passport');
 
-/* GET home page. */
+// Home page route
+// GET - /
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+// Google OAuth login route
+// GET - /auth/google
+router.get('/auth/google', passport.authenticate(
+  // Which passport strategy is being used?
+  'google',
+  {
+    // Requesting the user's profile and email
+    scope: ['profile', 'email'],
+    // Optionally force pick account every time
+    prompt: "select_account"
+  }
+));
+
+// Google OAuth callback route
+// GET - /oauth2callback
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    // TODO - Decide where to route
+    successRedirect: '/',
+    failureRedirect: '/'
+  }
+));
+
+// OAuth logout route
+// GET - /logout
+router.get('/logout', function(req, res){
+  req.logout(function() {
+    res.redirect('/movies');
+  });
 });
 
 module.exports = router;
