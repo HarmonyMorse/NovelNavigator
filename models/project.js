1e824425339951d330f633186ae1fb7d1c41c993
HarmonyMorse <harmonythedev@gmail.com>
2024-01-06 10:25:43 -0800

    Create Project schema.

--- models/project.js
+++ models/project.js
@@ -3,17 +3,37 @@ const Schema = mongoose.Schema;
 
 const projectSchema = new Schema(
     {
-        // someAttribute: {
-        //     type: someDataType,
-        //     required: someBoolean,
-        // },
-        // anotherAttribute: {
-        //     type: Number,
-        //     min: someMinNumber,
-        //     max: someMaxNumber,
-        //     default: someDefaultValue,
-        // },
-        // someSimpleAttribute: [someDataType],
+        // Assignments
+        user: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+            required: true,
+        },
+
+        // Character Details
+        characterCount: {
+            type: Number,
+            min: 0,
+            default: 0,
+        },
+        characters: [{
+            type: Schema.Types.ObjectId,
+            ref: 'Character',
+        }],
+
+        // Project Details
+        title: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            default: "No description provided.",
+        },
+        mediaType: {
+            type: String,
+            default: "Unspecified",
+        },
     },
     {
         timestamps: true,
